acaf5b82d49babee2cdaed51cb22cd94fb132d19
matteusviegas <msmatteusbr2@gmail.com>
2026-06-07 22:12:15 -0300

    Novo projeto

---
+++ src/main/java/br/com/globalsolution/agrosat/AgrosatApplication.java
@@ -0,0 +1,13 @@
+package br.com.globalsolution.agrosat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrosatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrosatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/globalsolution/agrosat/AgrosatApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.globalsolution.agrosat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrosatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
